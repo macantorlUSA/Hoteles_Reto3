3e7286f1a3704feda5080b3254f4712425aec7da
Miguel Cantor L <miguel.cantor.mt@correo.usa.edu.co>
2021-10-07 08:32:31 -0500

    ADD: Commit Inicial.

---
+++ src/main/java/mintic/misiontic/usa/ciclo3/grupo5/equipo2/SistemaHotelesAPI/SistemaHotelesApiApplication.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.usa.ciclo3.grupo5.equipo2.SistemaHotelesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHotelesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHotelesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mintic/misiontic/usa/ciclo3/grupo5/equipo2/SistemaHotelesAPI/SistemaHotelesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.usa.ciclo3.grupo5.equipo2.SistemaHotelesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHotelesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
