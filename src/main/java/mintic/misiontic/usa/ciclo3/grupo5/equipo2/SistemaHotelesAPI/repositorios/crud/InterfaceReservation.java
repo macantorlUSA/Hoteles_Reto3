f2cb9e66444051b823ca9b552b037f6b294d7d20
oscar fabian gomez jurado <oscar.gomez2.mt@correo.usa.edu.co>
2021-10-17 16:03:41 -0500

    version que subi a vm

--- src/main/java/mintic/misiontic/usa/ciclo3/grupo5/equipo2/SistemaHotelesAPI/repositorios/crud/InterfaceReservation.java
+++
@@ -1,8 +0,0 @@
-package mintic.misiontic.usa.ciclo3.grupo5.equipo2.SistemaHotelesAPI.repositorios.crud;
-
-import mintic.misiontic.usa.ciclo3.grupo5.equipo2.SistemaHotelesAPI.modelos.Reservation;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InterfaceReservation extends CrudRepository<Reservation, Integer> {
-
-}
